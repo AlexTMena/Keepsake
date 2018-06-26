d445a221419d641add976752998689419c2215b8
alex0108 <alexandro.mena0108@gmail.com>
2018-06-26 15:55:27 +0800

    fix filter option and portfolio position on different screens

--- script.js
+++ script.js
@@ -7,6 +7,22 @@
         var sLogoDesign = document.getElementById('showLogoDesign');
         var sWebDesign = document.getElementById('showWebDesign');
         
+        var printDesign = document.getElementsByClassName('print-design');
+        var logoDesign = document.getElementsByClassName('logo-design');
+        var webDesign = document.getElementsByClassName('web-design');
+
+        for(var x=0; x< printDesign.length; x++) {
+            printDesign[x].classList.remove('d-none');
+        }
+
+        for(var x=0; x< logoDesign.length; x++) {
+            logoDesign[x].classList.remove('d-none');
+        }
+
+        for(var x=0; x<webDesign.length; x++) {
+            webDesign[x].classList.remove('d-none');
+        }
+
         sAll.classList.add('active');
         sPrintDesign.classList.remove('active');
         sLogoDesign.classList.remove('active');
@@ -20,10 +36,28 @@
         var sLogoDesign = document.getElementById('showLogoDesign');
         var sWebDesign = document.getElementById('showWebDesign');
         
+        var printDesign = document.getElementsByClassName('print-design');
+        var logoDesign = document.getElementsByClassName('logo-design');
+        var webDesign = document.getElementsByClassName('web-design');
+        
+        for(var i=0; i< logoDesign.length; i++) {
+            logoDesign[i].classList.add('d-none');
+        }
+
+        for(var i=0; i< webDesign.length; i++) {
+            webDesign[i].classList.add('d-none')
+        }
+
+        for(var i=0; i< printDesign.length; i++) {
+            printDesign[i].classList.remove('d-none')
+        }
+        
+
         sPrintDesign.classList.add('active');
         sAll.classList.remove('active');
         sLogoDesign.classList.remove('active');
         sWebDesign.classList.remove('active');
+
     }
 
     function filterLogoDesign() {
@@ -32,6 +66,22 @@
         var sPrintDesign = document.getElementById('showPrintDesign');
         var sLogoDesign = document.getElementById('showLogoDesign');
         var sWebDesign = document.getElementById('showWebDesign');
+        
+        var printDesign = document.getElementsByClassName('print-design');
+        var logoDesign = document.getElementsByClassName('logo-design');
+        var webDesign = document.getElementsByClassName('web-design');
+
+        for(var x=0; x< printDesign.length; x++) {
+            printDesign[x].classList.add('d-none');
+        }
+
+        for(var x=0; x< logoDesign.length; x++) {
+            logoDesign[x].classList.remove('d-none');
+        }
+
+        for(var x=0; x< webDesign.length; x++) {
+            webDesign[x].classList.add('d-none');
+        }
 
         sLogoDesign.classList.add('active');
         sAll.classList.remove('active');
@@ -45,6 +95,22 @@
         var sPrintDesign = document.getElementById('showPrintDesign');
         var sLogoDesign = document.getElementById('showLogoDesign');
         var sWebDesign = document.getElementById('showWebDesign');
+        
+        var printDesign = document.getElementsByClassName('print-design');
+        var logoDesign = document.getElementsByClassName('logo-design');
+        var webDesign = document.getElementsByClassName('web-design');
+
+        for(var x=0; x< printDesign.length; x++) {
+            printDesign[x].classList.add('d-none');
+        }
+
+        for(var x=0; x<logoDesign.length; x++) {
+            logoDesign[x].classList.add('d-none');
+        }
+
+        for(var x=0; x< webDesign.length; x++) {
+            webDesign[x].classList.remove('d-none');
+        }
 
         sWebDesign.classList.add('active');
         sAll.classList.remove('active');
